21fda210cefd77e31d9411bfdd1fa71c40ff24ed
Prosol1 <prosoldata@gmail.com>
2023-06-17 11:16:44 -0400

    cdswfs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import { Analytics } from '@vercel/analytics/react';
+import Script from "next/script";
 
 import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        src= "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6469406013541860"
+      />
+      <Analytics />
       <Component {...pageProps} />
     </>
   )
