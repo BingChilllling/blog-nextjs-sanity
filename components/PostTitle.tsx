5d20370eda5b98fed4dc371b4c1f25a53c4901ba
Prosol1 <prosoldata@gmail.com>
2023-06-17 15:28:00 -0400

    jyiklolkjjhg

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="mb-5 mt-6 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-5xl">
+    <h1 className="mb-5 mt-6 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-6xl">
       {children}
     </h1>
   )
